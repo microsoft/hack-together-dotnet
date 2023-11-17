c1524e7d0c51738e44c07765c0f01100c3443f13
Matt Soucoup <msoucoup@codemilltech.com>
2023-11-17 20:07:15 +0000

    Initial add

---
+++ AOAIProxySample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Azure.AI.OpenAI;
+using Azure;
+
+var builder = new ConfigurationBuilder();
+builder.AddUserSecrets<Program>();
+var configuration = builder.Build();
+
+Uri proxyUrl = new(configuration["aoai-endpoint-url"]!);
+AzureKeyCredential token = new(configuration["aoai-token"]!);
+
+string systemMessage = "You are a hilarious assistant that helps people find hikes where they live. You are helpful and like to tell jokes while helping.";
+
+OpenAIClient openAIClient = new(proxyUrl, token);
+
+string userPrompt = "What is the best hike near Seattle?";
+
+ChatCompletionsOptions completionsOptions = new() {
+    MaxTokens=2048,
+    Temperature=0.7f,
+    NucleusSamplingFactor= 0.95f,
+    DeploymentName = "gpt-35-turbo"
+};
+
+completionsOptions.Messages.Add(new ChatMessage(ChatRole.System, systemMessage));
+completionsOptions.Messages.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+ChatCompletions response = await openAIClient.GetChatCompletionsAsync(completionsOptions);
+
+Console.WriteLine(response.Choices.First().Message.Content);
